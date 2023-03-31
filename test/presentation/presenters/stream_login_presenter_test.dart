8ad7f9c9755da411be6cb52e531e5dee8c5c747e
Willian Trevisol <willianbylla@hotmail.com>
2023-03-31 16:27:53 -0300

    feat: Added tests to ensure StreamLoginPresenter flow

--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -11,11 +11,13 @@ void main() {
   late StreamLoginPresenter systemUnderTest;
   late ValidationMock validation;
   late String email;
+  late String password;
 
   setUp(() {
     validation = ValidationMock();
     systemUnderTest = StreamLoginPresenter(validation: validation);
     email = faker.internet.email();
+    password = faker.internet.password();
   });
   
   test('Should call validation with correct email', () {
@@ -25,8 +27,77 @@ void main() {
 
   test('Should emit email error when validation fails', () {
     validation.mockValidationError(field: 'email', value: 'error');
-    expectLater(systemUnderTest.emailErrorStream, emits('error'));
+
+    systemUnderTest.emailErrorStream
+      .listen(expectAsync1((error) => expect(error, 'error')));
+    systemUnderTest.isFormValidStream
+      .listen(expectAsync1((isValid) => expect(isValid, false)));
 
     systemUnderTest.validateEmail(email);
+    systemUnderTest.validateEmail(email);
   });
+
+  test('Should emit null when email validation has no errors', () {
+    systemUnderTest.emailErrorStream
+      .listen(expectAsync1((error) => expect(error, null)));
+    systemUnderTest.isFormValidStream
+      .listen(expectAsync1((isValid) => expect(isValid, false)));
+
+    systemUnderTest.validateEmail(email);
+    systemUnderTest.validateEmail(email);
+  });
+
+  test('Should call validation with correct password', () {
+    systemUnderTest.validatePassword(password);
+    verify(() => validation.validate(field:'password', input: password)).called(1);
+  });
+
+  test('Should emit password error when validation fails', () {
+    validation.mockValidationError(field: 'password', value: 'error');
+
+    systemUnderTest.passwordErrorStream
+      .listen(expectAsync1((error) => expect(error, 'error')));
+    systemUnderTest.isFormValidStream
+      .listen(expectAsync1((isValid) => expect(isValid, false)));
+
+    systemUnderTest.validatePassword(password);
+    systemUnderTest.validatePassword(password);
+  });
+
+  test('Should emit null when password validation has no errors', () {
+    systemUnderTest.passwordErrorStream
+      .listen(expectAsync1((error) => expect(error, null)));
+    systemUnderTest.isFormValidStream
+      .listen(expectAsync1((isValid) => expect(isValid, false)));
+
+    systemUnderTest.validatePassword(password);
+    systemUnderTest.validatePassword(password);
+  });
+
+  test('Should emit email error when validation fails', () {
+    validation.mockValidationError(field: 'email', value: 'error');
+
+    systemUnderTest.emailErrorStream
+      .listen(expectAsync1((error) => expect(error, 'error')));
+    systemUnderTest.passwordErrorStream
+      .listen(expectAsync1((error) => expect(error, null)));
+    systemUnderTest.isFormValidStream
+      .listen(expectAsync1((isValid) => expect(isValid, false)));
+
+    systemUnderTest.validateEmail(email);
+    systemUnderTest.validateEmail(email);
+  });
+
+  test('Should enable authentication function', () async {
+    systemUnderTest.emailErrorStream
+      .listen(expectAsync1((error) => expect(error, null)));
+    systemUnderTest.passwordErrorStream
+      .listen(expectAsync1((error) => expect(error, null)));
+    expectLater(systemUnderTest.isFormValidStream, emitsInOrder([false, true]));
+
+    systemUnderTest.validateEmail(email);
+    await Future.delayed(Duration.zero);
+    systemUnderTest.validatePassword(password);
+  });
+
 }
