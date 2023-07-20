918846b461696aecd8ea93fa8460f553639c3d76
Willian Trevisol <willianbylla@hotmail.com>
2023-07-20 15:08:05 -0300

    feat: add ValidationBuilder

--- lib/main/factories/pages/login/login_validation_factory.dart
+++ lib/main/factories/pages/login/login_validation_factory.dart
@@ -1,13 +1,13 @@
 import 'package:clean_arch/presentation/protocols/protocols.dart';
 import 'package:clean_arch/validation/protocols/field_validation.dart';
 import 'package:clean_arch/validation/validators/validators.dart';
+import 'package:clean_arch/main/builders/builders.dart';
 
 Validation loginValidationFactory() {
   return ValidationComposite(loginValidationsFactory());
 }
 
 List<FieldValidation> loginValidationsFactory() => [
-  const RequiredFieldValidation('email'),
-  const EmailValidation('email'),
-  const RequiredFieldValidation('password'),
+  ...ValidationBuilder.field('email').required().email().build(),
+  ...ValidationBuilder.field('password').required().build()
 ];
