21eeac31a64ecaedff8793fb5522bc1b42bb5ad6
Willian Trevisol <willianbylla@hotmail.com>
2023-07-25 14:16:27 -0300

    feat: created add account usecase

--- lib/domain/entities/entities.dart
+++ lib/domain/entities/entities.dart
@@ -1,2 +1,3 @@
 export 'account.dart';
 export 'authentication_params.dart';
+export 'add_account_params.dart';
---
+++ lib/domain/usecases/add_account.dart
@@ -0,0 +1,5 @@
+import 'package:clean_arch/domain/entities/entities.dart';
+
+abstract class AddAccount {
+  Future<Account> add({required AddAccountParams params});
+}
