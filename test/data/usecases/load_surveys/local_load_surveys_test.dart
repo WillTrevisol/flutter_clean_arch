6413fffdd64809ea9b5a8a504731bcb3fbc763b7
Willian <willianbylla@hotmail.com>
2023-08-18 22:24:45 -0300

    feat: ensure LocalLoadSurveys throws UnexpectedError on empty list or null return from FetchCacheStorage

--- test/data/usecases/load_surveys/local_load_surveys_test.dart
+++ test/data/usecases/load_surveys/local_load_surveys_test.dart
@@ -1,8 +1,10 @@
-import 'package:clean_arch/data/entities/entities.dart';
-import 'package:clean_arch/domain/entities/survey.dart';
-import 'package:clean_arch/domain/usecases/usecases.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
+
+import 'package:clean_arch/domain/entities/survey.dart';
+import 'package:clean_arch/domain/helpers/helpers.dart';
+import 'package:clean_arch/domain/usecases/usecases.dart';
+import 'package:clean_arch/data/entities/entities.dart';
 
 import '../../../domain/mocks/mocks.dart';
 import '../../../infra/mocks/mocks.dart';
@@ -15,6 +17,9 @@ class LocalLoadSurveys implements LoadSurveys {
   @override
   Future<List<Survey>> load() async {
     final surveys = await fetchCacheStorage.fetch('surveys');
+    if (surveys == null || surveys?.isEmpty) {
+      throw DomainError.unexpected;
+    }
     return surveys.map<Survey>((survey) => LocalSurvey.fromMap(survey).toDomainEntity()).toList();
   }
 
@@ -31,7 +36,7 @@ class FetchCacheStorageMock extends Mock implements FetchCacheStorage {
   }
 
   When mockFetchCall() => when(() => fetch(any()));
-  void mockFetch({List<Map>? surveys}) => mockFetchCall().thenAnswer((_) async => surveys);
+  void mockFetch({dynamic surveys}) => mockFetchCall().thenAnswer((_) async => surveys);
 
 }
 
@@ -62,6 +67,20 @@ void main() {
       Survey(id: cacheSurveysMock[0]['id'], question: cacheSurveysMock[0]['question'], date: DateTime.utc(2023, 08, 18), didAnswer: cacheSurveysMock[0]['didAnswer']),
       Survey(id: cacheSurveysMock[1]['id'], question: cacheSurveysMock[1]['question'], date: DateTime.utc(2022, 08, 17), didAnswer: cacheSurveysMock[1]['didAnswer']),
     ]);
+  });
+
+  test('Should throw UnexpectedError if cache is empty', () async {
+    fetchCacheStorage.mockFetch(surveys: []);
+    final future = systemUnderTest.load();
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
+
+  test('Should throw UnexpectedError if cache is null', () async {
+    fetchCacheStorage.mockFetch(surveys: null);
+    final future = systemUnderTest.load();
+
+    expect(future, throwsA(DomainError.unexpected));
   });
 }
  
