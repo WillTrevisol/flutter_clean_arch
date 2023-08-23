db39370e44650d40874be3d529c20a8452089def
Willian <willianbylla@hotmail.com>
2023-08-23 15:10:11 -0300

    feat: ensure Composite calls local save with remote data

--- test/main/composites/remote_load_surveys_with_local_fallback_test.dart
+++ test/main/composites/remote_load_surveys_with_local_fallback_test.dart
@@ -8,13 +8,15 @@ import 'package:clean_arch/domain/usecases/usecases.dart';
 import '../../domain/mocks/mocks.dart';
 
 class RemoteLoadSurveysWithLocalFallback implements LoadSurveys {
-  RemoteLoadSurveysWithLocalFallback({required this.remote});
+  RemoteLoadSurveysWithLocalFallback({required this.remote, required this.local});
 
   final RemoteLoadSurveys remote;
+  final LocalLoadSurveys local;
 
   @override
   Future<List<Survey>> load() async {
-    await remote.load();
+    final surveys = await remote.load();
+    await local.save(surveys);
     return [];
   }
 
@@ -26,19 +28,37 @@ class RemoteLoadSurveysMock extends Mock implements RemoteLoadSurveys {
   void mockLoad(List<Survey> surveys) => mockLoadCall().thenAnswer((_) async => surveys);
 }
 
+class LocalLoadSurveysMock extends Mock implements LocalLoadSurveys {
+  LocalLoadSurveysMock() {
+    mockSave();
+  }
+  When mockSaveCall() => when(() => save(any()));
+  void mockSave() => mockSaveCall().thenAnswer((_) async => _);
+}
+
 void main() {
   late RemoteLoadSurveysWithLocalFallback systemUnderTest;
   late RemoteLoadSurveysMock remoteLoadSurveys;
+  late LocalLoadSurveysMock localLoadSurveys;
+  late List<Survey> surveys;
 
   setUp(() {
+    surveys = EntityFactory.listSurveys();
     remoteLoadSurveys = RemoteLoadSurveysMock();
-    systemUnderTest = RemoteLoadSurveysWithLocalFallback(remote: remoteLoadSurveys);
-    remoteLoadSurveys.mockLoad(EntityFactory.listSurveys());
+    localLoadSurveys = LocalLoadSurveysMock();
+    systemUnderTest = RemoteLoadSurveysWithLocalFallback(remote: remoteLoadSurveys, local: localLoadSurveys);
+    remoteLoadSurveys.mockLoad(surveys);
   });
 
   test('Should call remote load', () async {
     await systemUnderTest.load();
 
     verify(() => remoteLoadSurveys.load()).called(1);
+  });
+
+  test('Should call local save with remote data', () async {
+    await systemUnderTest.load();
+
+    verify(() => localLoadSurveys.save(surveys)).called(1);
   });
 }
